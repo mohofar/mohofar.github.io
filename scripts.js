c8cbe975359e8a3bd5d17f86cdec6f8f4c33e92c
Morteza Homayounfar <m.homayounfar@tudelft.nl>
2024-08-29 16:52:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,16 @@
 function openTab(evt, tabName) {
-    console.log("Tab Clicked: ", tabName); // Debugging line
     var i, tabcontent, tablinks;
+    // Hide all tab contents
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+    // Remove active class from all tabs
     tablinks = document.getElementsByClassName("tab");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
     }
+    // Show the clicked tab content
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active-tab";
 }
