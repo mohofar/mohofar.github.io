34c9e4c08638e90f99530251c0da940b62c31d9d
Morteza Homayounfar <m.homayounfar@tudelft.nl>
2024-08-29 15:53:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-// script.js
-
-function openTab(tabName) {
-    // Hide all tab contents
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";  
+        tabcontent[i].style.display = "none";
     }
-
-    // Remove the active class from all tabs
     tablinks = document.getElementsByClassName("tab");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
     }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
+    evt.currentTarget.className += " active-tab";
 }
-
-// Set default tab
-document.addEventListener("DOMContentLoaded", function() {
-    openTab('news');
-});
