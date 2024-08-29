72d69dd5cffe0351756ca118533036a747e2de90
Morteza Homayounfar <m.homayounfar@tudelft.nl>
2024-08-29 15:57:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active-tab", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active-tab";
-}
+// JavaScript code can be added here for interactive features
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Webpage loaded and ready.');
+});
